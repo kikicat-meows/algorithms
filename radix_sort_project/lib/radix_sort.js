4cc40a227ed07c026102bde3fa21fb34e643d35a
Helen Cheung <helenkei.cheung@gmail.com>
2020-02-04 15:50:42 -0800

    add counting_sort & binary_search

--- radix_sort_project/lib/radix_sort.js
+++ radix_sort_project/lib/radix_sort.js
@@ -1,4 +1,30 @@
+// What digit is at the given place value in num?
+function getDigitFrom(num, place) {
+    return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+};
+
+// const getDigitFrom = (num, place) => Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;;
+
+// How many digits are in num?
+function getIntLength(num) {
+    return (num === 0) ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+// How many digits does the integer with the most digits have?
+function getMaxDigits(nums) {
+
+}
+
+// Assume the above are pre-defined, explain only their inputs and what they do
+
 function radixSort(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+
+    if (arr.length <= 1) {
+        return arr;
+    }
 
 }
 
