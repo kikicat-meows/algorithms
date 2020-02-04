91bc923e79a163475a369281740f7d57e362257f
Helen Cheung <helenkei.cheung@gmail.com>
2020-02-04 14:49:28 -0800

    quick_sort solved

--- quick_sort_project/lib/quick_sort.js
+++ quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,16 @@
 function quickSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
 
+    let pivot = array.shift();
+    let left = array.filter( ele => ele < pivot );
+    let right = array.filter( ele => ele >= pivot );
+
+    let sortedLeft = quickSort(left);
+    let sortedRight = quickSort(right);
+
+    return sortedLeft.concat([pivot]).concat(sortedRight);
 }
 
 
